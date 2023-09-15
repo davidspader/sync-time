29f9917ca27aad0a00ee470a49002bd5cd433168
David <davidspader97@gmail.com>
2023-09-14 21:55:09 -0300

    alter image click to tab

--- index.py
+++ index.py
@@ -7,10 +7,12 @@ pyautogui.press('win')
 pyautogui.write("hora")
 pyautogui.press('enter')
 
-while not pyautogui.locateOnScreen('button-to-click.jpeg', confidence=0.5):
-    time.sleep(1)
+pyautogui.hotkey('tab')
+pyautogui.hotkey('tab')
+pyautogui.hotkey('tab')
 
-x, y, largura, altura = pyautogui.locateOnScreen('button-to-click.jpeg',confidence=0.5)
-pyautogui.click(x + largura/2, y + altura/2)
+pyautogui.hotkey('enter')
+
+time.sleep(1)
 
 pyautogui.hotkey('alt', 'f4')
